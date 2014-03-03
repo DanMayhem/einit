73d1998d4e6d155f92e4526f77f59aaa34fde435
Dan May <danmay@gmail.com>
2014-03-03 16:24:18 -0500

    x

---
+++ einit/static/js/main.js
@@ -0,0 +1,34 @@
+require.config({
+  baseUrl: '/vendor',
+  paths: {
+    js: '../js',
+    models: '../models',
+    views: '../views',
+    templates: '../templates'
+  },
+  shim: {
+    'marionette': {
+      deps: [ 'backbone' ],
+      exports: 'Marionette'
+    },
+    'backbone': {
+      deps: [ 'underscore', 'jquery' ],
+      exports: 'Backbone'
+    },
+    'underscore': {
+      exports: '_'
+    },
+    'jquery': {
+      exports: '$'
+    },
+    'handlebars': {
+      exports: 'Handlebars'
+    },
+    {
+    'bootstrap': {
+      exports: '$'
+    }
+    }
+  },
+  deps: [ 'js/app' ]
+});
