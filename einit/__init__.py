42add42048c9655bcc2431fdc85b12f536d0111f
Dan May <danmay@gmail.com>
2014-03-22 19:23:57 -0400

    templates

--- einit/__init__.py
+++ einit/__init__.py
@@ -2,8 +2,13 @@ import flask
 
 app = flask.Flask(__name__)
 
+#set debug mode as appropriate
+#load database
+#load secure token
+
 #register routes
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
-  return "Einit"
+  return flask.render_template("index.html")
