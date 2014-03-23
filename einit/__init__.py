814041331f34cb9545d073201f002ce6fa893a2b
Dan May <danmay@gmail.com>
2014-03-23 14:40:39 -0400

    x

--- einit/__init__.py
+++ einit/__init__.py
@@ -28,8 +28,9 @@ if not redis.exists('secret'):
   redis.set('secret_key',base64.b64encode(Crypto.Random.get_random_bytes(16)))
 app.config['SECRET_KEY'] = redis.get('secret_key')
 
-#require SSL unless in debug mode
-sslify = flask_sslify.SSLify(app)
+#other flask extensions
+sslify = flask_sslify.SSLify(app) #requires SSL
+bcrypt = flaskext.bcrypt.Bcrypt(app) #password digests
 
 #import models, views and helpers
 import einit.models
@@ -43,7 +44,22 @@ def index():
   return flask.render_template("index.html")
 
 #user routes
-@app.route('/signup', methods=['GET'])
-def signup():
-
+@app.route('/signup_form', methods=['GET'])
+def signup_form():
   return flask.render_template("signup.html",form=einit.views.SignUpForm())
+
+@app.route('/signup', methods=['POST'])
+def create_user():
+  form = einit.views.SignUpForm()
+  if not form.validate_on_submit():
+    u = einit.models.User(form.name.data, form.email.data, form.password.data)
+    db.session.add(u)
+    db.commit()
+    flask.redirect(flask.url_for(signup_form), code=302) #force method to get
+
+
+
+
+
+
+
--- einit/models.py
+++ einit/models.py
@@ -1,5 +1,6 @@
 import einit
-
+import Crypto.Random
+import hashlib
 
 my_db = einit.db
 
@@ -7,9 +8,17 @@ class User(my_db.Model):
   id = my_db.Column(my_db.Integer, primary_key = True)
   name = my_db.Column(my_db.String(64), index = True, unique = True)
   email = my_db.Column(my_db.String(120), index = True, unique = True)
-  password_digest = my_db.Column(my_db.String(64), index = False, unique = False)
-  session_digest = my_db.Column(my_db.String(64), index = True, unique = False)
+  password_digest = my_db.Column(my_db.String(64))
+  session_digest = my_db.Column(my_db.String(64), index = True, unique = True)
   role = my_db.Column(my_db.SmallInteger, default = 0)
+
+  def __init__(self, name, email, password):
+    self.name = name
+    self.email = email
+    self.password_digest = einit.bcrypt.generate_password_hash(password)
+    sha = hashlib.sha1()
+    sha.update(Crypto.Random.get_random_bytes(32))
+    self.session_digest = hashlib.sha1(Crypto.Random.get_random_bytes(32)).hexdigest()
 
   def __repr__(self):
     return '<User %r>' % (self.nickname)
