47d5557a847f9c305de8c0f50272858200bd5994
Dan May <danmay@gmail.com>
2014-03-22 20:04:48 -0400

    ssl and debug

--- einit/__init__.py
+++ einit/__init__.py
@@ -1,10 +1,22 @@
+import os
 import flask
+import flask.ext.sqlalchemy
+import flask_sslify
 
 app = flask.Flask(__name__)
 
 #set debug mode as appropriate
+if os.environ.has_key("DEBUG_EINIT"):
+  app.debug=True
+
 #load database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"sqllite://")
+app.config['SQLALCHEMY_MIGRATE_REPO'] = os.path.join(os.path.abspath(os.path.split(os.path.dirname(__file__))[0]),'migrations')
+db = flask.ext.sqlalchemy.SQLAlchemy(app)
 #load secure token
+
+#require SSL unless in debug mode
+sslify = flask_sslify.SSLify(app)
 
 #register routes
 @app.route('/')
