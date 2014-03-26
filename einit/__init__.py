d48191f2c5aaa60529490855a4e9888803bd0cb0
Dan May <danmay@gmail.com>
2014-03-26 13:08:30 -0400

    changed secret key to env var rather than redis

--- einit/__init__.py
+++ einit/__init__.py
@@ -1,9 +1,5 @@
 import os
 
-import redis
-import Crypto.Random
-import base64
-
 import flask
 import flask.ext.sqlalchemy
 import flask_sslify
@@ -20,13 +16,9 @@ app.config['CSRF_ENABLED'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"sqllite://")
 app.config['SQLALCHEMY_MIGRATE_REPO'] = os.path.join(os.path.abspath(os.path.split(os.path.dirname(__file__))[0]),'migrations')
 db = flask.ext.sqlalchemy.SQLAlchemy(app)
-redis = redis.from_url(os.environ.get('REDISTOGO_URL'))
 
 #load secure token
-if not redis.exists('secret'):
-  #add secret key to redis DB
-  redis.set('secret_key',base64.b64encode(Crypto.Random.get_random_bytes(16)))
-app.config['SECRET_KEY'] = redis.get('secret_key')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 #other flask extensions
 sslify = flask_sslify.SSLify(app) #requires SSL
